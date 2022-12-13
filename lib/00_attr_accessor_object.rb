6aa3276ae73757b531576713caaf46a2c8eeaca1
“Emmett <emmett.wechsler11@gmail.com>
2022-12-13 17:07:13 -0500

    Finished accessors

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -2,9 +2,8 @@ class AttrAccessorObject
   def self.my_attr_accessor(*names)
 
     names.each do |name|
-      method_name = name.to_s
-        define_method(name) { "@#{name} = #{instance_variable_get(name)}" }
-        define_method(name) { "@#{name}" }
+      define_method(name) { instance_variable_get("@#{name}") }
+      define_method("#{name}=") { |ele| self.instance_variable_set("@#{name}", ele) }
     end
       
 
